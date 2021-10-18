b9ae97ec15d40850d023d02f9029f605097d3e63
Jamie Faselt <jamiefaselt@u.boisestate.edu>
2021-10-18 12:47:31 -0600

    new temp raster for rasterizing other datasets woo

--- HSI/HSI_Revisit.R
+++ HSI/HSI_Revisit.R
@@ -27,7 +27,7 @@ mt_fws <- st_read("/Users/jamiefaselt/Research/Data//MT Data/MT_FWS.shp")
 mt_CMR <- mt_fws %>% 
   filter(., ORGNAME=="CHARLES M. RUSSELL NATIONAL WILDLIFE REFUGE",  drop=TRUE)
 mt_NPS <- st_read("/Users/jamiefaselt/Research/Data/NationalParkServiceAdminBoundaries_shp 2")
-mt_NPS %>% 
+yellowstone <- mt_NPS %>% 
   filter(., UNIT_NAME=="Yellowstone National Park",  drop=TRUE)
 ### need to go back and combine all of these polygons into one then create buffer on southern border then make this my extent/resolution raster to match my future rasters to (new on 10.13)
 # combine 
@@ -37,8 +37,9 @@ reservations <- mt_reservations %>% st_transform(crs=st_crs(hsi)) %>%
   st_make_valid()
 cmr <- mt_CMR %>% st_transform(crs=st_crs(hsi)) %>% 
   st_make_valid()
-yellowstone <- mt_NPS %>% st_transform(crs=st_crs(hsi)) %>% 
+yellowstone <- yellowstone %>% st_transform(crs=st_crs(hsi)) %>% 
   st_make_valid()
+mt <- mt %>% st_transform(crs=st_crs(hsi))
 
 #combine them into one shapefile
 #reservations<- subset(reservations, select=c(geometry, NAME))
@@ -59,12 +60,20 @@ ext <- st_bbox(all.nodes)
 bbox <- spPolygons(extent(all.nodes), crs=crs(hsi))
 bbox.buffer <- buffer(bbox, 50000)
 
-poly <- st_as_sfc(st_bbox(c(xmin = st_bbox(all.nodes)[[1]], xmax = st_bbox(all.nodes)[[3]], ymax = st_bbox(all.nodes)[[4]], ymin = st_bbox(bbox.buffer)[[2]]), crs = st_crs(hsi)), res=540)
-plot(poly)
+poly <- st_as_sfc(st_bbox(c(xmin = st_bbox(mt)[[1]], xmax = st_bbox(mt)[[3]], ymax = st_bbox(mt)[[4]], ymin = st_bbox(all.nodes)[[2]]-50000), crs = st_crs(hsi)))
 r <- raster(crs= proj4string(as(poly, "Spatial")), ext=raster::extent(as(poly, "Spatial")), resolution= 540)
 hsi.crop <- crop(hsi, as(poly, "Spatial"))
-
+plot(mt$geometry)
 extent(r)
 extent(all.nodes)
+st_crs(r)==st_crs(hsi)
 
+writeRaster(r, "temp_rstr.tif")
+writeRaster(hsi.crop,"hsi_crop_540")
 
+plot(poly)
+plot(all.nodes, add=TRUE)
+
+plot(mt$geometry)
+plot(poly, add=TRUE)
+plot(all.nodes, add=TRUE)
